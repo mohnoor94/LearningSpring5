cb34ccccb4949cda32491decf57f93593cce85d7
Mohammad Noor <moh.noor94@gmail.com>
2018-07-24 18:07:41 +0300

    Start implementing the game logic and add the 'beans.xml' file to the core module

--- guess-the-number-game/core/src/main/java/com/abukhleif/Main.java
+++ guess-the-number-game/core/src/main/java/com/abukhleif/Main.java
@@ -2,13 +2,29 @@ package com.abukhleif;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class Main {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final String CONFIG_LOCATION = "beans.xml";
+
 
     public static void main(String[] args) {
         log.info("Guess the Number Game");
+
+        // Create Context (Container)
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+
+        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+
+        // Use bean
+        log.info("randomNumber => {}", numberGenerator.next());
+        log.info("maxNumber => {}", numberGenerator.getMaxNumber());
+
+        // Close Context (Container)
+        context.close();
     }
 }
---
+++ guess-the-number-game/core/src/main/java/com/abukhleif/NumberGenerator.java
@@ -0,0 +1,8 @@
+package com.abukhleif;
+
+public interface NumberGenerator {
+
+    int next();
+
+    int getMaxNumber();
+}
